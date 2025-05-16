ac90c35fd7ad394ec1d04ac06e4cad516da16e28
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-05-16 16:29:13 -0600

    skeleton for the game is done and responsive
    finished the comment for the javascript

--- js/app.js
+++ js/app.js
@@ -3,4 +3,10 @@
 
 // what am i trying to do?
 
-// user 
+// user gets to the website
+// there's a rules page - welcome to 'simple battleship' , how to play: click the squares to guess where the battleship is , you'll know when you hit ;)
+
+// when user hits, an icon of a bomb pops up in the space
+
+// theres a reset button & a "back to the rules" page that takes you back to the first page
+// if they click play, the game restarts again just like the first time
