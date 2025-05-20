f91424769578d3969c5f604dce98f5da7adf86c0
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-05-20 10:47:20 -0600

    Update practice.js

--- js/practice.js
+++ js/practice.js
@@ -11,34 +11,32 @@ let squares = document.querySelectorAll(".square");
 console.log(squares);
 
 
-let gameOver = false;
-
+let gameAlreadyWon = false;
 
 for (let i = 0; i < squares.length; i++) {
+    debugger
     squares[i].addEventListener("click", () => {
 
-        if (gameOver) {
-            return;
+        if (gameAlreadyWon === true) {
+            return; //exit the function
         }
         
         if (i === battleshipPosition) {
+            gameAlreadyWon = true;
             squares[i].style.backgroundColor = "green";
-            gameOver = true;
 
-            // ask Mike why this doesn't work
-            // alert("You Hit the Battleship!");
             let winText = document.getElementById("winText");
             winText.innerText = "You Hit the Battleship!";
         } else {
             squares[i].style.backgroundColor = "red";
         }
     });
-
+    
 }
 
 
 
-// Makes the page relaod/restart game... idk if this is the correct way to do it...  looks like it's refreshing the entire page
+// Makes the page relaod/restart game... idk if this is the correct way to do it...  looks like it's refreshing the entire pae
 document.getElementById('restartButton').addEventListener('click', () => {
     window.location.reload();
 });
